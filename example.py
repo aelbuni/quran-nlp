a087c8158ff90fbab0db104505ea4de3621ee01c
aelbuni <abdulrhman.elbuni@gmail.com>
2020-05-10 01:32:36 -0400

    - Encapsulated the model learning and deployment in class - Made the class user friendly to use - Still more work need to be done to allow users to control the model through passing parameters

---
+++ example.py
@@ -0,0 +1,20 @@
+from quran_nlp.quran_nlp import QuranContextToWords
+import matplotlib.pyplot as plt 
+
+# Instantiate a new obkect
+contextToWords = QuranContextToWords()
+
+# Start the learning process and cache the model for use
+contextToWords.process_quran_book()
+
+# Start using the model and play with visualization
+# You can generate multiple plots through matlibplot as follows
+
+plt.figure(1)
+plt.subplot(211)
+word = 'الجنة'
+contextToWords.print_similar_word_cloud(word, 50)
+
+plt.subplot(212)
+word = 'النار'
+contextToWords.print_similar_word_cloud(word, 50)
